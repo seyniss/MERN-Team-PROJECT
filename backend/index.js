98fd57a53e5e7067b3a441866a33d0e09695ff6f
seyniss <shepunk2@kbu.ac.kr>
2025-08-19 11:58:11 +0900

    초기세팅
    서버(backend)/클라이언트(front)

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config(); 
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Mongodb연결 성공"))
+  .catch((err) => console.log("연결 실패", err));
+
+
