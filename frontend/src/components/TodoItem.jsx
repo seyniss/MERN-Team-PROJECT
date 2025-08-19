da410bb973b69cd39dc2cd83ac7ab762da6d8ed4
gueon7866 <lsg7866@gmail.com>
2025-08-19 12:44:34 +0900

    update
    1

---
+++ frontend/src/components/TodoItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./TodoItem.css"
+const TodoItem = ({todo ,onDelete}) => {
+  return (
+    <div className='TodoItem'>
+        <input type="checkbox" readOnly/>
+        <div className="content">{todo.text}</div>
+        <div className="date">{new Date(`${todo.date}`).toLocaleDateString()}</div>
+        <div className="btn-wrap">
+            <button className="updateBtn">수정</button>
+            <button className="deleteBtn" 
+            onClick={()=>onDelete(todo._id)}
+            >삭제</button>
+        </div>
+    </div>
+  )
+}
+
+export default TodoItem
---
+++ frontend/src/components/TodoList.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './TodoList.css'
+import TodoItem from './TodoItem'
+const TodoList = ({todos,onDelete}) => {
+  return (
+    <div className='TodoList'>
+        <h4>Todo List 🌱</h4>
+        <input type="text" placeholder='검색어를 입력하세요' />
+        <div className="todos-wrapper">
+          {todos.map((todo,i)=>(
+
+            <TodoItem key={i} todo={todo} onDelete={onDelete}/>
+          ))}
+        
+        </div>
+    </div>
+  )
+}
+
+export default TodoList
