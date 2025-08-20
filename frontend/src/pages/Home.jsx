4b556c9cd07ae4af5d069b510266ab00606c395b
lyb5382 <lyb538202@gmail.com>
2025-08-20 12:15:46 +0900

    j

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import About from './About'
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <TodoEditor />
+            <TodoList />
+        </div>
+    )
+}
+
+export default Home
